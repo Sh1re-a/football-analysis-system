1d691acadfebc967d005999fe749e367b002bd52
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-03-21 01:59:20 +0100

    Problemet kvartstår

--- ImageOpenCV/image.js
+++ ImageOpenCV/image.js
@@ -1,22 +1,41 @@
-let src = cv.imread('canvasInput');
-let dst = new cv.Mat();
-// To distinguish the input and output, we graying the image.
-// You can try different conversions.
-cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-cv.imshow('canvasOutput', dst);
-src.delete();
-dst.delete();
-
-
 let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
 inputElement.addEventListener('change', (e) => {
   imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
+document.getElementById('circlesButton').onclick = function() {
+  this.disabled = true;
+  document.body.classList.add('loading');
+
+  // circle detection code
+  // circle detection code
+let srcMat = cv.imread('imageCanvas');
+let displayMat = srcMat.clone();
+let circlesMat = new cv.Mat();
+cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
+  this.disabled = false;
+  document.body.classList.remove('loading');
+  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
+};
+
+
+for (let i = 0; i < circlesMat.cols; ++i) {
+  let x = circlesMat.data32F[i * 3];
+  let y = circlesMat.data32F[i * 3 + 1];
+  let radius = circlesMat.data32F[i * 3 + 2];
+  let center = new cv.Point(x, y);
+
+  // draw circles
+  cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
+}
+
 imgElement.onload = function() {
   let mat = cv.imread(imgElement);
+  
   cv.imshow('canvasOutput', mat);
+  
+
   mat.delete();
 };
 function onOpenCvReady() {
